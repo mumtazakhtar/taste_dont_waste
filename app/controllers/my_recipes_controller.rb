0dbe9ee690e392c43f488999b9347e542bb623ab
nikki dekerf <nikkidekerf@gmail.com>
2023-01-31 16:18:05 +0100

    pundit has iitem policy and my recipes policy

--- app/controllers/my_recipes_controller.rb
+++ app/controllers/my_recipes_controller.rb
@@ -1,4 +1,29 @@
 class MyRecipesController < ApplicationController
-  skip_after_action :verify_authorized
+  # skip_after_action :verify_authorized
+  before_action :authorize_my_recipe
 
+  def index
+    @my_recipes = policy_scope(MyRecipe)
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def authorize_my_recipe
+    authorize @my_recipe
+  end
 end
