1c58b7487233bf47f6666819b07cbaa763e0de74
Sari Vos <vos.sari@gmail.com>
2023-01-31 12:26:10 +0100

    routes done cookbook deleted

--- db/migrate/20230130155259_create_recipes.rb
+++ db/migrate/20230130155259_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :title
-      t.string :ingedients
+      t.string :ingredients
       t.string :description
       t.integer :cookingTime
 
--- db/migrate/20230130161103_create_cookbooks.rb
+++
@@ -1,9 +0,0 @@
-class CreateCookbooks < ActiveRecord::Migration[7.0]
-  def change
-    create_table :cookbooks do |t|
-      t.references :recipe, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
