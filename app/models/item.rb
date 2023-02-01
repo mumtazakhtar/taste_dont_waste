26dc385bb4ec317892ae07dcc3d5d9a05efd290a
nikki dekerf <nikkidekerf@gmail.com>
2023-02-01 18:03:14 +0100

    validations for item, user and my recipe

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :product, :bestByDate, :stock, presence: true
+  # Validate best by date with alert?
+  # Stock > 0?
 end
--- app/models/my_recipe.rb
+++ app/models/my_recipe.rb
@@ -1,4 +1,6 @@
 class MyRecipe < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
+
+  validates :title, :ingredients, :description, :cookingTime, presence: true
 end
