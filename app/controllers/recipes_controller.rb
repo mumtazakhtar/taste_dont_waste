7d277dc2e9614f47cbdafe3c40649988f4325e68
Sari Vos <vos.sari@gmail.com>
2023-02-01 15:13:07 +0100

    added favorits to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,19 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def favorite
+    @recipe = Recipe.find(params[:id])
+    current_user.favorite(@recipe)
+  end
+
+  def unfavorite
+    @recipe = Recipe.find(params[:id])
+    current_user.unfavorite(@recipe)
+  end
+
+  def all_favorited
+    current_user.all_favorited
+  end
+
 end
