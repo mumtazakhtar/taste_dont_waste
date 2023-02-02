475c9aba37a109bc1a0f8d0f1d22c15b3658dee4
nikki dekerf <nikkidekerf@gmail.com>
2023-02-02 10:28:17 +0100

    added presence validations for item, my recipe, recipe and user model

--- app/models/my_recipe.rb
+++ app/models/my_recipe.rb
@@ -3,4 +3,6 @@ class MyRecipe < ApplicationRecord
   belongs_to :user
 
   validates :title, :ingredients, :description, :cookingTime, presence: true
+  # Validations to think about later:
+  # Should items be destroyed when user is deleted
 end
