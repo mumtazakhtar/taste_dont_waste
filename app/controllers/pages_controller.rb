a6ae2989e9eb7eedc9419e1bf50b6f331f0bd84e
Sari Vos <vos.sari@gmail.com>
2023-02-02 16:48:51 +0100

    added the links in the navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   end
 
   def cookbook
-    @cookbook_recipes = user.all_favorited + user.my_recipes
+    @cookbook_recipes = current_user.all_favorited + current_user.my_recipes
   end
 
 end
