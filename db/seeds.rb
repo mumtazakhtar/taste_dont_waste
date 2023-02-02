3bcf9bf536fcfb8435a22b8b0dece0a2214a8a0f
mumtaz mohammed <mumtazmohammed08@gmail.com>
2023-02-02 15:15:28 +0100

    added api to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+# Define your Spoonacular API key
+api_key = "d8eacd8b06424ccdb8ce3f3ecb9fb5a2"
+
+puts "Start creating recipes..."
+recipe_ids = []
+recipe_tags = ["dinner", "vegetarian", 'lunch']
+# Define the URL for accessing the Spoonacular API
+recipe_tags.each do |recipe_tag|
+  puts "Requesting #{recipe_tag}"
+  url = "https://api.spoonacular.com/recipes/random?tag=#{recipe_tag}&number=2&apiKey=#{api_key}"
+  dinner_recipes_data = JSON.parse(URI.open(url).read)
+  recipe_ids += dinner_recipes_data["recipes"].map { |recipe| recipe["id"] }
+end
+recipe_ids.uniq
+sleep(1)
+# looping through the recipe id to create diffferent dinner recipes
+recipe_ids.each do |recipe_id|
+  url2 = "https://api.spoonacular.com/recipes/#{recipe_id}/information?includeNutrition=false&apiKey=#{api_key}"
+  # Get the JSON data from the Spoonacular API
+  recipe_data = JSON.parse(URI.open(url2).read)
+  title = recipe_data["title"]
+  readyInMinutes = recipe_data["readyInMinutes"]
+  image = recipe_data["image"]
+  sleep(1)
+
+  # Ingredients for recipe
+  url3 = "https://api.spoonacular.com/recipes/#{recipe_id}/ingredientWidget.json?apiKey=#{api_key}"
+
+  ingredients_list = JSON.parse(URI.open(url3).read)
+  ingredients = []
+  ingredients << ingredients_list["ingredients"].map { |ingredient| ingredient["name"] }
+  sleep(1)
+
+  # instructions for 716429 id recipe
+  url4 = "https://api.spoonacular.com/recipes/#{recipe_id}/analyzedInstructions?apiKey=#{api_key}"
+  instructions_list = JSON.parse(URI.open(url4).read)
+
+  instruction_steps = [] # steps for instructions
+  instructions = instructions_list[0]['steps']
+  instructions.map { |step| instruction_steps << step['step'] }
+
+  # set image through cloudinary
+  file = URI.open(image)
+
+  # create a new recipe and fill it with the api data
+  recipe = Recipe.new(title: title, ingredients: ingredients, description: instruction_steps, cookingTime: readyInMinutes)
+  recipe.photo.attach(io: file, filename: "#{title}.jpg", content_type: "image/jpg")
+  recipe.save
+  puts "Created recipe ##{recipe_id}: #{title}."
+  sleep(1)
+end
+recipe_amount = Recipe.count
+puts "Finished creating recipes! You have a total of #{recipe_amount}"
