3ade9d1144e1f478e1ce97aae92b7d32704e01b4
mumtaz mohammed <mumtazmohammed08@gmail.com>
2023-02-06 13:09:57 +0100

    changed the form

--- app/javascript/controllers/search_recipes_controller.js
+++ app/javascript/controllers/search_recipes_controller.js
@@ -2,11 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 
 // Connects to data-controller="search-recipes"
-export default class extends Controller {
-  connect() {
-    console.log("Hello from the controller")
-  }
-}
+// export default class extends Controller {
+//   connect() {
+//     console.log("Hello from the controller")
+//   }
+// }
 
 // Connects to data-controller="search-recipes"
 export default class extends Controller {
