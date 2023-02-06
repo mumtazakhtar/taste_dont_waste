4786f4c265d56bd11f49965ef87223a41aa97209
Sari Vos <vos.sari@gmail.com>
2023-02-06 17:37:31 +0100

    fixed the popup show and ordering of items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,10 @@ class PagesController < ApplicationController
   def cookbook
     @cookbook_recipes = current_user.all_favorited + current_user.my_recipes
     @items = Item.all
+    @items = Item.order(bestByDate: :asc)
     @recipes_favorite = current_user.all_favorited
     @recipes_created = current_user.my_recipes
+    # @days = DateTime.now - Date.bestByDate
   end
+
 end
