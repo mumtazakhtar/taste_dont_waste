9f156bddee906423665898aadee5bfb3d25e681c
mumtaz mohammed <mumtazmohammed08@gmail.com>
2023-02-06 09:50:43 +0100

    added popup

--- app/javascript/controllers/search_recipes_controller.js
+++ app/javascript/controllers/search_recipes_controller.js
@@ -1,8 +1,8 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="search-recipes"
-export default class extends Controller {
-  connect() {
-    console.log("Hello from the controller")
-  }
-}
+// // Connects to data-controller="search-recipes"
+// export default class extends Controller {
+//   connect() {
+//     console.log("Hello from the controller")
+//   }
+// }
