f02f83c312eae6a08efca5b41941d8f88fb6abfc
nikki dekerf <nikkidekerf@gmail.com>
2023-02-06 12:15:14 +0100

    trying to retreive value from the ingredient

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,8 @@ class ItemsController < ApplicationController
 
   def index
     @items = policy_scope(Item)
-    @items = Item.find_by(user_id: user.id).all
+    # @items = Item.find_by(user_id: user.id).all
+    @items = Item.all
     # add a sort by function
   end
 
--- app/javascript/controllers/search_recipes_controller.js
+++ app/javascript/controllers/search_recipes_controller.js
@@ -3,14 +3,32 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-recipes"
 export default class extends Controller {
 
-  static targets = [ "form", "test" ]
+  static targets = [ "form", "ingredients", "ingredient" ]
+  static values = ["ingredient"]
 
   connect() {
-    console.log(this.formTarget.value)
+    // console.log(this.formTarget.value)
+
   }
 
   addIngredient() {
-    this.testTarget.innerText = "Bingo!"
+    console.log("Ingredient added")
+  }
+
+  removeIngredient() {
+    console.log("Remove button clicked")
+    console.log(this.ingredientTarget.innerText)
+    // console.log(this.ingredientTarget.value)
+    // console.log(this.searchRecipesTarget.value)
+    // console.log(this.ingredientTarget.value)
+    console.log(this.ingredientTarget.value)
+    // console.log(this.searchIngredientTarget.value)
+
+    // console.log(this.formTarget.value)
+    // const newQuery = [this.formTarget.value]
+    // console.log(newQuery)
+    // const x = newQuery.shift();
+    // console.log(`variable x value: ${x}`);
   }
 
 
